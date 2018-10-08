85804e9f90c45025d232d004b513a43af4016fbe
Andrey Shklyaruk <13mailg@gmail.com>
2018-10-08 12:22:28 +0300

    Tasks simple CRUD

--- routes/tasks.js
+++ routes/tasks.js
@@ -4,13 +4,71 @@ var router = express.Router();
 var mongojs = require('mongojs');
 var db = mongojs('mongodb://andrey:andrey1312@ds245680.mlab.com:45680/tasklist', ['tasks']);
 
-router.get('/tasks', function (req, res, nex) {
+// Get All Tasks
+router.get('/tasks', function (req, res, next) {
   db.tasks.find(function(err, tasks){
-    if (err) {
-      res.send(err);
-    }
+    if (err) res.send(err);
     res.json(tasks);
   });
+});
+
+// Get Single Task
+router.get('/task/:id', function (req, res, next) {
+  db.tasks.findOne({_id: mongojs.ObjectID(req.params.id)}, function (err, task) {
+    if (err) res.send(err);
+    res.json(task);
+  });
+});
+
+// Save Task
+router.post('/task', function(req, res, next){
+  var task = req.body;
+  if(!task.title || (task.isDone + '')){
+    res.status(400);
+    res.json({
+      "error": "Bad DATA"
+    });
+  }else{
+    db.task.save(task, function(err, task){
+      if (err) res.send(err);
+      res.json(task);
+    });
+  }
+});
+
+// Delete Task
+router.delete('/task/:id', function (req, res, next) {
+  db.tasks.remove({ _id: mongojs.ObjectID(req.params.id) }, function (err, task) {
+    if (err) res.send(err);
+    res.json(task);
+  });
+});
+
+// Update Task
+router.put('/task/:id', function (req, res, next) {
+  var task = req.body;
+  var updTask = {};
+
+  if (task.isDone) {
+    updTask.isDone = task.isDone;
+  }
+
+  if (task.title) {
+    updTask.title = task.title;
+  }
+
+  if (!updTask) {
+    res.status(400);
+    res.json({
+      "error": "Bad DATA"
+    });
+  } else {
+    db.tasks.update({ _id: mongojs.ObjectID(req.params.id) }, updTask, {}, function (err, task) {
+      if (err) res.send(err);
+      res.json(task);
+    });
+  }
+
 });
 
 module.exports = router;
